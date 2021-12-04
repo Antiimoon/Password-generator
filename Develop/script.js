6aeb3630b41ebf7596117834bf723a94dba470b8
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2021-12-03 20:01:19 -0800

    prompts and alerts added

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function getPassword() {
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(),./<>?-_=+";
+  var passwordLength = 8 < 128;
+  var password = "";
+
+  var passwordLength=prompt("How many characters would you like your password to have?")
+  alert("Click OK if you would like to include special characters?")
+  alert("Click OK if you would like to include uppercase characters?")
+  alert("Click OK if you would like to include lowercase characters?")
+  alert("Click OK if you would like to include number characters?")
+  
+  for(let i=0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber + 1);
+  }
+  document.getElementById("password").value = password;
+ 
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,4 +34,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", getPassword);
